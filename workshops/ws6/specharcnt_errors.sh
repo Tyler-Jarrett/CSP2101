5948d827a4717c5b728220ca916e84b17c823839
Tyler <tylerschwehr04@gmail.com>
2025-04-29 09:01:55 +0800

    Updated workshop 6 progress, downloaded portfolio 2 files

--- workshops/ws6/specharcnt_errors.sh
+++ workshops/ws6/specharcnt_errors.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
+# Function to count the number of special characters
 count_special_chars() {
+    # Take the first parameter and name it input_str
     local input_str="$1"
-    local special_chars=$(echo "$input_str" | grep -o '[^a-z0-9]' | tr -d '\n')
+    # Grep for any non alphanumeric characters, deleting the line end
+    local special_chars=$(echo "$input_str" | grep -o '[^a-zA-Z0-9]' | tr -d '\n')
+    # Count the number of special characters found
     local count=${#special_chars}
+    # Return the characters and the number of characters
     echo "$special_chars" "$count"
 }
 
+# Store the first argument as file variable
 file="$1"
 
+# Check that the file exists
 if [ ! -f "$file" ]; then
     echo "File does not exist."
     exit 1
 else
+    # Print the header for the table
     printf "%-20s %-10s %-10s \n" "STRING" "COUNT" "MATCHES"
-    while read line || [ -n "$line" ]; do
+    # For every line in the file
+    while read -r line || [ -n "$line" ]; do
+        # Call the count special characters function, passing in the line
         result=$(count_special_chars "$line")
-        specials=$(echo "$result" | cut -d' ' -f1)
-        num_specials=$(echo "$result" | cut -d' ' -f2)
-        printf "%-20s %-10s %-10s \n" "$line" "$num_specials" "$specials"
+        # Cut the result, only taking the special characters
+        specials=$(echo "$result" | cut -d' ' -f 1)
+        # Cut the result, only taking the count of characters
+        num_specials=$(echo "$result" | cut -d' ' -f 2)
+        # If there is more than 0 special characters, print the line
+        if [[ num_specials -ne 0 ]]; then
+            printf "%-20s %-10s %-10s \n" "$line" "$num_specials" "$specials"
+        fi
     done < "$file"
 fi
 
--- workshops/ws6/strmaths.sh
+++ workshops/ws6/strmaths.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Check to see if the file exists
 if [ ! -f "$1" ]; then
     echo "File does not exist!"
     exit 1
 fi
 
+# Not a correct if statement, should be square brackets and using the -ne operator
 if (( "$2" != "-a" || "$2" != "-m" )); then
     echo "You must supply either -a for add or -m for multiply."
     exit 1
@@ -14,20 +16,31 @@ matches=""
 result=0
 matchcnt=0
 charcnt=0
-while read line; do
+
+# This will ignore the final line in the file, because you don't capture lines without line end markers
+while read line; do    
     charcnt=${#line}
+    # Wrong exit condition, either count up or change it to less than 0
     for (( i=$charcnt; i>0; i-- )); do
+        # This should be using $i
         char=$(echo "$line" | cut -c $charcnt)
+
         if [[ $char =~ [0-9] ]]; then
-        matches+="$char"
+            matches+="$char"
         fi
+        # You already have a decrementing integer, use that instead
         ((charcnt--))
     done
-    # echo "$matches"
+
+    # echo "$matches" - What is this for??
     matchcnt=${#matches}
+
     if [[ $matchcnt -gt 0 ]]; then
+        # Should be inclusive of 0
         for (( i=$matchcnt; i>0; i-- )); do
+            # Same problem as above, should be using $i
             temp=$(echo "$matches" | cut -c $matchcnt)
+            # Wrong comparison, should be using the -eq
             if [ "$2" == "-a" ]; then
                 result=$(($result + $temp))
             elif [ "$2" == "-m" ]; then
